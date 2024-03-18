ed5abee52d5d5a9854f74ae76d2fd2b1c5318991
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-03-18 23:11:27 +0100

    first commit

--- src/main/java/tn/esprit/pidev/controller/controller.java
+++ src/main/java/tn/esprit/pidev/controller/controller.java
@@ -1,4 +1,4 @@
-package tn.esprit.pidev;
+package tn.esprit.pidev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
