b4d91eabd995182d29225a4ebc27a178bb2ebd44
unknown <yassin.mezni@esprit.tn>
2024-03-22 14:43:12 +0100

    CRUD

--- src/main/java/tn/esprit/pidev/services/GestionReclamationImpl.java
+++ src/main/java/tn/esprit/pidev/services/GestionReclamationImpl.java
@@ -1,4 +1,38 @@
 package tn.esprit.pidev.services;
 
-public class GestionReclamationImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tn.esprit.pidev.entities.Reclamtion;
+import tn.esprit.pidev.repository.IReclamationRepository;
+
+import java.util.List;
+
+@Service
+public class GestionReclamationImpl implements IGestionReclamation {
+    @Autowired
+    IReclamationRepository iReclamationRepository;
+    @Override
+    public List<Reclamtion> retrieveAllReclamation() {
+        return iReclamationRepository.findAll();
+    }
+
+    @Override
+    public Reclamtion addReclamation(Reclamtion reclamtion) {
+        return iReclamationRepository.save(reclamtion);
+    }
+
+    @Override
+    public Reclamtion updateReclamtion(Reclamtion reclamtion) {
+        return iReclamationRepository.save(reclamtion);
+    }
+
+    @Override
+    public Reclamtion RetrieveReclamation(long idrec) {
+        return iReclamationRepository.findById(idrec).get() ;
+    }
+
+    @Override
+    public void deleteReclamation(long idrec) {
+iReclamationRepository.deleteById(idrec);
+    }
 }
--- src/main/java/tn/esprit/pidev/services/IGestionReclamation.java
+++ src/main/java/tn/esprit/pidev/services/IGestionReclamation.java
@@ -5,5 +5,12 @@ import tn.esprit.pidev.entities.Reclamtion;
 import java.util.List;
 
 public interface IGestionReclamation {
+List<Reclamtion>retrieveAllReclamation();
+Reclamtion addReclamation(Reclamtion reclamtion);
+Reclamtion updateReclamtion(Reclamtion reclamtion);
+
+Reclamtion RetrieveReclamation(long idrec);
+void deleteReclamation(long idrec);
+
 
 }
