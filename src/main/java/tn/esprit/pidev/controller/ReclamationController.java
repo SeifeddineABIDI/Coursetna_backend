71588461d0c3a0dd6e060626f64f74e583deafec
unknown <yassin.mezni@esprit.tn>
2024-03-30 22:53:42 +0100

    add reclamation

--- src/main/java/tn/esprit/pidev/controller/ReclamationController.java
+++ src/main/java/tn/esprit/pidev/controller/ReclamationController.java
@@ -2,6 +2,7 @@ package tn.esprit.pidev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esprit.pidev.entities.Reclamtion;
@@ -16,11 +17,17 @@ public class ReclamationController {
     @Autowired
     IGestionReclamation iGestionReclamation;
 
-    
+    //*************---Retrieve all reclamation *******////
     @GetMapping("/GetAllReclamtions")
     public List<Reclamtion> getall()
     {
         return  iGestionReclamation.retrieveAllReclamation();
+    }
+
+    //******************* ---addReclamation--- ************
+    @PostMapping("addReclamation")
+    public Reclamtion addReclamation(Reclamtion reclamtion){
+        return  iGestionReclamation.addReclamation(reclamtion);
     }
 
 
