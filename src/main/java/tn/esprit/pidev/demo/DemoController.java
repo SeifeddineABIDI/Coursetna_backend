48bfb06106e013b2e2b1339dd0631dd6d5ae13b3
SeifeddineABIDI <seifeddine.abidi@esprit.tn>
2024-04-06 04:57:37 +0100

    edited controllers

--- src/main/java/tn/esprit/pidev/demo/DemoController.java
+++ src/main/java/tn/esprit/pidev/demo/DemoController.java
@@ -16,7 +16,12 @@ public class DemoController {
         return ResponseEntity.ok("Hello from secured endpoint");
     }
 
-
+    @GetMapping("/ping")
+    public String test() {
+        try {
+            return "Welcome";
+        } catch (Exception e) {
+            return "ghalet";
         }
     }
 }
