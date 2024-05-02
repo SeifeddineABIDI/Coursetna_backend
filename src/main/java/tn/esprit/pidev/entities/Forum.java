ffb74210b9d00e10d442d61d23dff8546a890de7
Souid-Houssem <84329777+Souid-Houssem@users.noreply.github.com>
2024-05-03 00:52:18 +0100

    forum-entity

---
+++ src/main/java/tn/esprit/pidev/entities/Forum.java
@@ -0,0 +1,33 @@
+package com.example.picloud.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Forum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String emailAddress;
+    private String phoneNumber;
+    private String address;
+    private String coverLetter; // Cover letter text
+    private String resumeCV; // Path to the uploaded resume/CV file
+
+    // Ajoutez les constructeurs, getters et setters
+}
